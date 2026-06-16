b6b40b18e3fd23f984f46f6ffb2a1d888902c6e3
karister <karister@qq.com>
2026-06-16 20:47:15 +0800

    fix uni h5 dev server port

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import uniPlugin from '@dcloudio/vite-plugin-uni'
 const uni = typeof uniPlugin === 'function' ? uniPlugin : uniPlugin.default
 
 export default defineConfig({
-  plugins: [uni()]
+  plugins: [uni()],
+  server: {
+    host: '127.0.0.1',
+    port: 5173,
+    strictPort: true
+  }
 })
